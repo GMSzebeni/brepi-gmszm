edf3cfa7b4d6e5b35fad0f78a1b99f3849114531
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2023-09-25 23:03:20 +0200

    updated beers.componenet.ts to load beers and added pagination

--- src/app/beers/beers.component.ts
+++ src/app/beers/beers.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BeersDataService } from '../beers-data.service';
 
 @Component({
   selector: 'app-beers',
   templateUrl: './beers.component.html',
   styleUrls: ['./beers.component.less']
 })
-export class BeersComponent {
+export class BeersComponent implements OnInit{
+  beers: any[] = [];
+  currentPage = 1;
+  itemsPerPage = 6;
+  status: string = 'loading';
 
+  constructor(private beersDataService: BeersDataService) {}
+
+  ngOnInit(): void {
+    this.loadBeers();
+  }
+
+  loadBeers(): void {
+    this.beersDataService
+      .getBeers(this.currentPage, this.itemsPerPage)
+        .subscribe(data => {
+          this.beers = data;
+        });
+  }
+
+  nextPage(): void {
+    if (this.currentPage < 55) {
+      this.currentPage++;
+      this.loadBeers();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadBeers();
+    }
+  }
 }
