1f33be5119cdb5a59fa1ccb3afeab19e1c1d5060
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2023-09-25 11:05:37 +0200

    beers component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeersComponent } from './beers/beers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeersComponent
   ],
   imports: [
     BrowserModule,
